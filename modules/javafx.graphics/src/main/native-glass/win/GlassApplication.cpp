771b198d5ae893fadceab6f0c64525b31f93db43
Joeri Sykora <joeri.sykora@gluonhq.com>
2019-12-17 14:19:30 +0100

    add some debug

--- modules/javafx.graphics/src/main/native-glass/win/GlassApplication.cpp
+++ modules/javafx.graphics/src/main/native-glass/win/GlassApplication.cpp
@@ -358,14 +358,18 @@ extern "C" {
 #ifdef STATIC_BUILD
 JNIEXPORT jint JNICALL JNI_OnLoad_glass(JavaVM *jvm, void *reserved)
 {
+fprintf(stderr, "[JSDBG] glass A\n");
 #ifdef JNI_VERSION_1_8
     //min. returned JNI_VERSION required by JDK8 for builtin libraries
     JNIEnv *env;
     if (jvm->GetEnv((void **)&env, JNI_VERSION_1_8) != JNI_OK) {
+        fprintf(stderr, "[JSDBG] glass B\n");
         return JNI_VERSION_1_4;
     }
+    fprintf(stderr, "[JSDBG] glass C\n");
     return JNI_VERSION_1_8;
 #else
+    fprintf(stderr, "[JSDBG] glass D\n");
     return JNI_VERSION_1_4;
 #endif
 }
