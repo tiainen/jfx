1f360c6a46738e7097f689d2a2b7c691cde41704
Joeri Sykora <joeri.sykora@gluonhq.com>
2020-01-10 09:32:41 +0100

    remove fprintf statements

--- modules/javafx.graphics/src/main/native-prism/Helpers.c
+++ modules/javafx.graphics/src/main/native-prism/Helpers.c
@@ -40,18 +40,14 @@
 
 JNIEXPORT jint JNICALL
 JNI_OnLoad_prism_common(JavaVM* vm, void* reserved) {
-fprintf(stderr, "[JSDBG] prism_common A\n");
 #ifdef JNI_VERSION_1_8
     //min. returned JNI_VERSION required by JDK8 for builtin libraries
     JNIEnv *env;
     if ((*vm)->GetEnv(vm, (void **)&env, JNI_VERSION_1_8) != JNI_OK) {
-        fprintf(stderr, "[JSDBG] prism_common B\n");
         return JNI_VERSION_1_4;
     }
-    fprintf(stderr, "[JSDBG] prism_common C\n");
     return JNI_VERSION_1_8;
 #else
-    fprintf(stderr, "[JSDBG] prism_common D\n");
     return JNI_VERSION_1_4;
 #endif
 }
